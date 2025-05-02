9e0d73081120ec8aa70119ff82e9785eb0b9ac2d
praneeth guota <nitinkumariit4@gmail.com>
2025-05-02 11:57:04 +0530

    home page completed

--- src/components/MortgageFetch.tsx
+++ src/components/MortgageFetch.tsx
@@ -7,14 +7,14 @@ function MortgageFetch() {
     {loanOption.map((item,index)=>
       <div key={index}>
         <div className="text-2xl font-bold">{item.title}</div>
-        <div className="grid grid-cols-2 shadow-2xl p-3 px-6 md:p-6 md:px-24 m-3 rounded-2xl" >
-            <div className="col-span-2 text-xs text-center">PURCHASE - 30 YEAR FIXED</div>
+        <div className="grid grid-cols-2 gap-8 shadow-2xl p-3 px-6 md:p-6 md:px-24 m-3 rounded-2xl bg-white" >
+            <div className="col-span-2 text-sm items-center text-center">PURCHASE - 30 YEAR FIXED</div>
             <div><div className="text-xs">RATE</div><div className="text-card font-bold text-lg">7.63%</div></div>
             <div><div className="text-xs">TW change</div><div className="flex items-center justify-center md:justify-around"><img src={arrow} className="bg-green-600 w-4 rounded-full -rotate-90" alt="" /><span>1.14%</span></div></div>
             <div><div className="text-xs">APR</div><div className="text-card font-bold text-lg">8.613%</div></div>
             <div><div className="text-xs">TW change</div><div className="flex items-center md:justify-around justify-center"><img src={arrow} className="bg-red-600 w-4 rounded-full rotate-90" alt="" /><span>1.14%</span></div></div>
         </div>
-        <div className="grid grid-cols-2 shadow-2xl p-3 px-6 md:p-6 md:px-24 rounded-2xl" >
+        <div className="grid grid-cols-2 gap-8 shadow-2xl p-3 bg-white px-6 md:p-6 md:px-24 m-3 rounded-2xl" >
             <div className="col-span-2 text-xs">REFINANCE - 30 YEAR FIXED</div>
             <div><div className="text-xs">RATE</div><div className="text-card font-bold text-lg">7.63%</div></div>
             <div><div className="text-xs">TW change</div><div className="flex items-center md:justify-around justify-center"><img src={arrow} className="bg-green-600 w-4 rounded-full -rotate-90" alt="" /><span>1.14%</span></div></div>
