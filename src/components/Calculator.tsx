42c2b6a90f868167702344bcb31501d5b7a0f8f3
praneeth guota <nitinkumariit4@gmail.com>
2025-05-12 10:30:24 +0530

    added calculations to calculator

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import { useState } from 'react';
 import AffordCalculator from './AffordCalculator';
 import ConventionalCalculator from './ConventionalCalculator';
 import FHACalculator from './FHACalculator';
